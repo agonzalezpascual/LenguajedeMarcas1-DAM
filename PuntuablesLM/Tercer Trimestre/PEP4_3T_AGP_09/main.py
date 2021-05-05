b7affeb0eeb9b3c110d414b19ee38b9fc9055dd3
agonzalezpascual <agonzalezpascual@safareyes.es>
2021-05-05 14:24:58 +0200

    Puntuable 4.2

--- PuntuablesLM/Tercer Trimestre/PEP4_3T_AGP_09/main.py
+++ PuntuablesLM/Tercer Trimestre/PEP4_3T_AGP_09/main.py
@@ -1,4 +1,8 @@
+import sys
+
+import catch as catch
 import mysql.connector
+
 
 class Producto:
 
@@ -29,21 +33,108 @@ class Producto:
     def precio(self):
         return self.__precio
 
-db = mysql.connector.connect(host="localhost",user="root",passwd="" ,database="Ecommerce")
+    def __str__(self):
+        return f"Producto(id = {self.__id}, nombre: {self.__nombre}, código de barras: {self.__codigoBarra}, cantidad: {self.__cantidad}, precio: {self.__precio})"
 
-mycursor = db.cursor()
 
-#mycursor.execute("CREATE TABLE Productos (id smallint PRIMARY KEY auto_increment, nombre VARCHAR(35), codigoBarra CHAR(8), cantidad smallint unsigned, precio float)")
+def recogeFila(id):
 
-nombrePro = "Piza"
+        mycursor.execute(f"SELECT * FROM Productos WHERE id = {id}")
+        for x in mycursor:
+            p = Producto(x[0], x[1], x[2], x[3], x[4])
+            print(p)
+
+
+
+def recogeTabla():
+    try:
+        mycursor.execute("SELECT * FROM Productos")
+        l = []
+        for x in mycursor:
+            p = Producto(x[0], x[1], x[2], x[3], x[4])
+            print(p)
+    except:
+        print("Asegurese de haber introducido datos válidos")
+
+
+def introduceFila(id, nombrePro, codBarrPro, cantPro, precioProd):
+    try:
+        mycursor.execute(
+            f"INSERT INTO Productos (id, nombre, codigoBarra, cantidad, precio) VALUES ({id},'{nombrePro}','{codBarrPro}',{cantPro},{precioProd})")
+    except:
+        print("Asegurese de haber introducido datos válidos")
+
+def actualizaFila(id, nombrePro, codBarrPro, cantPro, precioProd):
+    try:
+        mycursor.execute(
+            f"UPDATE Productos set nombre = '{nombrePro}', codigoBarra = '{codBarrPro}', cantidad = {cantPro}, precio = {precioProd} where id = {id}")
+    except:
+        print("Asegurese de haber introducido datos válidos")
+
+def borraFila(id):
+    try:
+        mycursor.execute(f"DELETE FROM Productos where id = {id}")
+    except:
+        print("Asegurese de haber introducido datos válidos")
+
+
+# mycursor.execute("CREATE TABLE Productos (id smallint PRIMARY KEY auto_increment, nombre VARCHAR(35), codigoBarra CHAR(8), cantidad smallint unsigned, precio float)")
+
+"""nombrePro = "Piza"
 codBarrPro = "12674478"
 cantPro = 1
-precioProd = 2.5
+precioProd = 2.5"""
 
-#mycursor.execute(f"INSERT INTO Productos (id, nombre, codigoBarra, cantidad, precio) VALUES (3,'{nombrePro}','{codBarrPro}',{cantPro},{precioProd})")
+# mycursor.execute(f"INSERT INTO Productos (id, nombre, codigoBarra, cantidad, precio) VALUES (3,'{nombrePro}','{codBarrPro}',{cantPro},{precioProd})")
+# introduceFila(4,"Pan","87654321",1,1.65)
 
-mycursor.execute("SELECT * FROM Productos WHERE id = 1")
+db = mysql.connector.connect(host="localhost", user="root", passwd="", database="Ecommerce")
 
-for x in mycursor:
-    print(x)
+mycursor = db.cursor()
+while(True):
+    print("\n1) Consulta de un producto\n2) Consulta de todos los productos\n3) Inserción de un producto\n4) Actualización de un producto\n5) Borrado de un producto\n6) Salir\n")
+    opc = input("Introduzca la opción: ")
+    print()
+    if opc == "1":
+        PK = int(input("Introduzca la id del producto"))
+        recogeFila(PK)
+        db.commit()
+    elif opc == "2":
+        recogeTabla()
+        db.commit()
+    elif opc == "3":
+        try:
+            PK = int(input("Introduzca la id del producto: "))
+            nomProd = input("Introduzca el nombre del producto: ")
+            codBarr = input("Introduzca el código de barras del producto: ")
+            contProd = int(input("Introduzca la cantidad del producto: "))
+            precProd = float(input("Introduzca el precio del producto: "))
+            introduceFila(PK, nomProd, codBarr, contProd, precProd)
+        except:
+            print("\nAsegúrese de haber introducido datos válidos")
+
+        db.commit()
+    elif opc == "4":
+        try:
+            PK = int(input("Introduzca la id del producto: "))
+            nomProd = input("Introduzca el nombre del producto: ")
+            codBarr = input("Introduzca el código de barras del producto: ")
+            contProd = int(input("Introduzca la cantidad del producto: "))
+            precProd = float(input("Introduzca el precio del producto: "))
+            actualizaFila(PK, nomProd, codBarr, contProd, precProd)
+        except:
+            print("\nAsegúrese de haber introducido datos válidos")
+        db.commit()
+    elif opc == "5":
+        PK = int(input("Introduzca la id del producto: "))
+        borraFila(PK)
+        db.commit()
+    elif opc == "6":
+        print("Hasta la próxima")
+        sys.exit(0)
+    else:
+        print("Introduzca una opción correcta\n")
+
+"""for x in mycursor:
+    print(x)"""
 db.commit()
